0afda28a621f2d4d746523ba125e7a1dc24c1809
winstonliu <winstonl95@hotmail.com>
2016-04-05 09:33:34 -0400

    Written, untested.

--- find_runtime.py
+++ find_runtime.py
@@ -14,7 +14,9 @@ def runtime(func):
     print(end - start)
     return wrapper
 
-def time_func(func):
+def time_func(func, rollout=0):
+    timings = []
+
     # The board we are playing on
     board = reversi.make_board(4);
     # Whose turn it is to play
@@ -33,9 +35,34 @@ def time_func(func):
         passed = False;
 
         # Play a regular move.
-        move, score = func(board, cur_move);
+        start = time.clock()
+        if rollout == 0:
+            move, score = func(board, cur_move);
+        else:
+            move, score = func(board, cur_move, num_rollouts=rollout);
+        end = time.clock()
+        timings.append(end-start)
+
         reversi.make_move(board, move, cur_move);
         cur_move = reversi.get_other_player(cur_move);
 
-if __name__ == '__main__':
+    return timings
 
+if __name__ == '__main__':
+    # Run alpha-beta and minimax
+    for f in [alpha_beta, minimax]:
+        timings = time_func(f.get_move)
+        avg_timing = sum(timings)/float(len(timings))
+        print("{} has avg runtime of {}".format(f.__name__, avg_timing))
+
+    # Run monte_carlo
+    for rollout in [2,20,200,2000]:
+        rollout_time = []
+        # Do x runs per rollout
+        for k in range(100):
+            monte_carlo.all_nodes = {} # Reset nodes
+            timings = time_func(f.get_move, rollout)
+            rollout_time.append(sum(timings)/float(len(timings)))
+
+        avg_time = sum(rollout_time)/float(len(rollout_time))
+        print("Monte Carlo {} rollouts has avg runtime of {}".format(rollout, avg_time))
